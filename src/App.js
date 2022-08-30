8d911fad36fe0b20f420bb769bdf4129dd352284
gerald <oforige@gmail.com>
2022-08-30 16:24:31 +0000

    added created sign in and sign up pages, routed them all

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="">This is a new app</div>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<SignIn/>}></Route>
+        <Route path='/signin' element={<SignIn/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+
+      </Routes>
+    </div>
   );
 }
 
