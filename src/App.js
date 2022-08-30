78e55f739036b99b6647d0404622fffe6630c107
gerald <oforige@gmail.com>
2022-08-30 23:27:03 +0000

    made changes to the header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Route path='/' element={<SignIn/>}></Route>
         <Route path='/signin' element={<SignIn/>}></Route>
         <Route path='/signup' element={<SignUp/>}></Route>
-
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
       </Routes>
     </div>
   );
