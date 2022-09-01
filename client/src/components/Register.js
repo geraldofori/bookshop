73eb102083b8f26084aa8a5fad5fb545b3e9a3a4
gerald <oforige@gmail.com>
2022-09-01 23:32:06 +0000

    onsubmit methods and onchage methods added

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React,{useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -26,13 +26,19 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Register() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(false);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    // console.log({
+    //   email: data.get('email'),
+    //   password: data.get('password'),
+    // });
   };
 
   return (
@@ -53,29 +59,8 @@ export default function Register() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" noValidate onSubmit={(e)=> handleSubmit(e)} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
@@ -83,6 +68,8 @@ export default function Register() {
                   id="email"
                   label="Email Address"
                   name="email"
+                  value={email}
+                  onChange={(e)=> setEmail(e.target.value)}
                   autoComplete="email"
                 />
               </Grid>
@@ -91,6 +78,8 @@ export default function Register() {
                   required
                   fullWidth
                   name="password"
+                  value={password}
+                  onChange={(e)=> setPassword(e.target.value)}
                   label="Password"
                   type="password"
                   id="password"
@@ -109,6 +98,7 @@ export default function Register() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={(e)=> handleSubmit(e)}
             >
               Sign Up
             </Button>
